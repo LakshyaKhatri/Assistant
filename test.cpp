d9445adf30adf656127ffc81b577c33b3f710eff
LakshyaKhatri <lakshyak67@gmail.com>
2017-10-01 21:43:22 +0530

    Adding test file working of Jarvis Few errors encountered

---
+++ test.cpp
@@ -0,0 +1,96 @@
+using namespace std;
+#include<iostream>
+#include<stdlib.h>
+#include<termios.h>
+#include<fstream>
+#include<string.h>
+#include<unistd.h>
+
+ifstream iCommand;								//object of class fstream , will perform the input operations from file to program
+char finalAction[150];
+
+class Jarvis
+{
+	private:
+		char order[100];
+		char action[150];
+	public:
+		Jarvis()
+		{
+			order[0] = '\0';
+			action[100] = '\0'; 
+		}
+		void getOrder();
+		void searchOrder();
+		void perform();
+};
+
+void Jarvis::getOrder()
+{
+	fflush(stdin);
+	fflush(0);
+	cin.getline(order,99);
+
+	if(strcmpi(order,"exit") == 0 || strcmpi(order,"bye") == 0)
+	{
+		cout<<"See you Later !";
+		system("espeak -g3 -s150 -v english_wmids \"See you later !\"");
+		sleep(1.5);
+		exit(0);
+	}
+}
+
+void Jarvis::searchOrder()
+{
+	Jarvis temp;
+	iCommand.seekg(0,ios::beg);								//takes file pointer to the beginning
+	
+	//Until EOF is reached do this :-
+	while(iCommand.read((char *) &temp, sizeof(temp)))
+	{
+		if(strcmpi(temp.order,this->order) == 0)
+		{
+			//makes the action to be performed
+			strcpy(finalAction,"espeak -g3 -s150 -v english_wmids \"");
+			strcat(finalAction,this->action);
+			strcat(finalAction,"\"");
+
+			return;
+		}
+
+	}
+
+	//If order not found
+	system("espeak -g3 -s150 -v english_wmids \"I Cannot understand what you say !\"");
+	sleep(1.2);
+	system("espeak -g3 -s150 -v english_wmids \"I will learn this thing soon.\"");
+}
+
+void Jarvis::perform()
+{
+	cout<<action;
+	system(finalAction);
+	finalAction[0] = '\0';
+}
+
+void main()
+{
+	Jarivs jarvis1;
+	iCommand.open("Commands.txt",std::in | std::binary);
+	if(iCommand.fail())
+	{
+		cout<<"Error in opening Commands.txt\n";
+		exit(0);
+	}
+	
+	cout<<"Hello! How can I help you ?";
+	system("espeak -g3 -s150 -v english_wmids \"Hello ! How can I help you ?\"");
+	while(1)
+	{
+		jarvis1.getOrder();
+		jarvis1.searchOrder();
+		jarvis1.perform();
+	}
+
+
+}
