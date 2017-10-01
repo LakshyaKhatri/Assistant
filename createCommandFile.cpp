ede62f5b5c37aea2893eb603bc4a3cec774452a8
LakshyaKhatri <lakshyak67@gmail.com>
2017-10-01 19:40:35 +0530

    Add files for manupulating basic commands

---
+++ createCommandFile.cpp
@@ -0,0 +1,81 @@
+
+/* 
+ * Note that the commands are stored in ascending order of 
+ * orders .So, that the commands can be easily accessed 
+ * using Binary or Interpolation search
+ */
+
+using namespace std;
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+
+fstream interface;
+
+class Command
+{
+	private:
+		char order[100];
+		char action[100];
+	public:
+		Command()
+		{
+			order[0] = '\0';
+			action[0] = '\0';
+		}
+		void getData();
+		void writeToFile();
+		void showCommandFile();
+};
+
+void Command::getData()
+{
+	cout<<"Enter the order : ";
+	cin.getline(order,99);				//take a complete sentence from the keyboard until "ENTER" key is pressed ( defined in iostream ).
+	cout<<"Enter the action : ";
+	cin.getline(action,99);	
+}
+
+void Command::writeToFile()
+{
+	interface.seekg(0,ios::end);								//takes file pointer to the end .
+	interface.write((char *) this,sizeof(*this));
+}
+
+void Command::showCommandFile()
+{
+	interface.seekg(0);			//takes file pointer to the beginning
+	
+	while(interface.read( (char *) this , sizeof(*this)))
+		cout<<this->order<<"    "<<this->action<<endl;
+}
+
+int main()
+{
+	interface.open("Commands.txt" , ios::ate | ios::in | ios::out | ios::binary); 		 	/* opens Commands.txt in
+																							 * concatenate mode | input mode | output mode | binary mode 
+																							 * respectively
+																							 */
+	Command obj;
+	char more = 'y';
+
+	if(interface.fail())			//exits the execution if there is any error while opening the file
+	{
+		cout<<"Error in opening the file\n";
+		exit(1);
+	}
+	
+	do
+	{
+		obj.getData();
+		obj.writeToFile();
+		cout<<"Command Inserted"<<endl;
+		cout<<"Enter more commands ..? (y/n) : "<<endl;
+		cin.get(more);
+	
+	}while(more == 'y');
+
+	obj.showCommandFile();
+	interface.close();
+	return 0;
+}
