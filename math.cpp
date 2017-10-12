5f936f58d9c171b574b0ab5ed9518233f194b3ee
LakshyaKhatri <lakshyak67@gmail.com>
2017-10-12 11:05:51 +0530

    Assistant learned how to perform math operations but unable to handle numbers with decimals.

--- math.cpp
+++ math.cpp
@@ -13,88 +13,121 @@
 #define DIVIDE 1214
 #define MULTIPLY 1215
 
+//for reading math commands from file
 fstream ioMath;
+
+//for checking that the command found or not
 short flag = NOTFOUND;
+
+//for pushing and popping words of given orders
 LinkedList sentence;
+
+//for storing numbers given by users and giving their results
 long  num1 = 0,num2 = 0,result = 0;
 
+//for checking how many numbers are there on the input stream
+int totalNumbersFound = 0;
 class MathCommand
 {
 	private:
 		char order[100];
 		unsigned int actionCode;
 	public:
+		void setOrder(char*);
 		void parseOrder();
 		void searchOrder();
 		void perform();
-		MathCommand(char*);
+		MathCommand()
+		{
+			order[0] = '\0';
+			actionCode = 0;
+		}
 };
 
-MathCommand::MathCommand(char *o)
+void MathCommand::setOrder(char *o)
 {
 	strcpy(order,o);
 }
 
 void MathCommand::parseOrder()
 {
-	int i = 0, j = 0, k = 1;
+	int i = 0, j = 0;
 	char word[20],number[20];
+	
 	do
 	{
-		word[j++] = order[i]; 
+		word[j++] = order[i++]; 
 		if(order[i] == ' ' || order[i] == '\0')
 		{
 			word[j] = '\0';
-			if((word[0] >= '0' || word[0] == '-') && word[j-1] <= '9' && k == 1)
+			if((word[0] >= '0' || word[0] == '-') && word[j-1] <= '9' && totalNumbersFound == 0)
 			{
 				num1 = atol(word);
-				k++;
+				totalNumbersFound++;
 			}
 
-			else if((word[0] >= '0' || word[0] == '-') && word[j-1] <= '9' && k == 2)
+			//totalNumbersFound == 1 signifies that one number is found 
+			//now take the next number in the 
+			//second veriable.
+			else if((word[0] >= '0' || word[0] == '-') && word[j-1] <= '9' && totalNumbersFound == 1)
 			{
 				num2 = atol(word);
-				k++;
+				totalNumbersFound++;
 			}
 
-			if( word[0] == '+' || word[0] == 'A' || word[0] == 'a' || word[0] == 's' || word[0] == 'S' || word[0] == 't' || word[0] == 'T' || word[0] == 'A' ||   word[0] == 'i' || word[0] == 'I' || word[0] == '-' || word[0] == 'r' || word[0] == 'R' || word[0] == 'm' || word[0] == 'M' || word[0] == 'd' || word[0] == 'D' ||   word[0] == 'x' || word[0] == '*' || word[0] == 'B' || word[0] == 'b' )
+			//if any word starts with the same letter from which the action has started then push it in the linked list. 
+			if( word[0] == '+' || word[0] == 'A' || word[0] == 'a' || word[0] == 's' || word[0] == 'S' || word[0] == 't' || word[0] == 'T' || word[0] == 'i' || word[0] == 'I' || (word[0] == '-' && word[1] == '\0') || word[0] == 'r' || word[0] == 'R' || word[0] == 'm' || word[0] == 'M' || word[0] == 'd' || word[0] == 'D' ||   word[0] == 'x' || word[0] == '*')
 			{
 				sentence.insert(word);
 			}
-
-			j = 0;
+			
+			i++;			//for skipping space
+			j = 0;			//for a whole new word
 		}
 
-	}while(order[i++] != '\0');
+	}while(order[i] != '\0');
 
 }
 
 void MathCommand::searchOrder()
 {
-	char *word;
-	MathCommand *temp;
+	char word[20];
+	MathCommand temp;
 	ioMath.open("MathCommandFile.txt", ios::ate | ios::out | ios::in );
-	ioMath.seekg(ios::beg);
-	while(ioMath.read((char *) &temp, sizeof(temp)))
+	if(ioMath.fail())
 	{
-		if(stricmp(word,order) == 0)
+		cout<<"Error in opening the file";
+		exit(0);
+	}
+	
+	while(sentence.getFirst(word) == 1)
+	{
+		ioMath.clear();
+		ioMath.seekg(ios::beg);
+		while(ioMath.read((char *) &temp, sizeof(temp)))
 		{
-			flag = FOUND;
-			switch(actionCode)
+			if(stricmp(word,temp.order) == 0)
 			{
-				case ADD:
-					result = num1 + num2;
-					break;
-				case SUBTRACT:
-					result = num1 - num2;
-					break;
-				case DIVIDE:
-					result = num1 / num2;
-					break;
-				case MULTIPLY:
-					result = num1 * num2;
-					break;
+				flag = FOUND;
+				switch(temp.actionCode)
+				{
+					case ADD:
+						result = num1 + num2;
+						break;
+					case SUBTRACT:
+						result = num1 - num2;
+						break;
+					case DIVIDE:
+						result = num1 / num2;
+						break;
+					case MULTIPLY:
+						result = num1 * num2;
+						break;
+				}
 			}
+
+			if(flag == FOUND)
+				break;
 		}
 
 		if(flag == FOUND)
@@ -107,7 +140,7 @@ void MathCommand::perform()
 	char finalTask[100];
 	finalTask[0] = '\0';
 	char finalResult[20];
-
+	
 	if(flag == FOUND)
 	{
 		strcpy(finalTask,"espeak -g3 -s150 -a200 -v english_wmids");
@@ -122,8 +155,16 @@ void MathCommand::perform()
 
 int main()
 {
-	MathCommand obj((char *)"Add 876 and 728");
+	MathCommand obj;
+	obj.setOrder((char *)"multiply 99 with 2.47");
 	obj.parseOrder();
+	
+	//if no number is detected in the input stream.
+	if(totalNumbersFound == 0)
+	{
+		flag = NOTFOUND;
+		return 0;
+	}
 	obj.searchOrder();
 	obj.perform();
 	return 0;
